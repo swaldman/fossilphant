d6f3ab41598e08ac02922ccbd8797d209a2954eb
Steve Waldman <swaldman@mchange.com>
2023-09-23 21:21:56 -0400

    Generate a fossilphant scala-cli script, with fully configurable by command-line args.

--- fossilphant/src/com/mchange/fossilphant/FossilphantConfig.scala
+++ fossilphant/src/com/mchange/fossilphant/FossilphantConfig.scala
@@ -1,20 +1,21 @@
 package com.mchange.fossilphant
 
+import scala.collection.*
 import java.time.ZoneId
 
 case class FossilphantConfig(
-  archivePath             : Option[String]     = None,
-  mainTitle               : Option[String]     = None,
-  mainTagline             : Option[String]     = None,
-  overrideDisplayName     : Option[String]     = None,
-  newSelfUrl              : Option[String]     = None,
-  themeName               : String             = "shatter",
-  themeConfig             : Map[String,String] = Map.empty,
-  pageLength              : Int                = 20, // for themes that support paging
-  contentTransformer      : ContentTransformer = identity,
-  toFollowersAsPublic     : Boolean            = false,
-  sensitiveAsPublic       : Boolean            = false,
-  suppressLinksToOriginal : Boolean            = false,
-  timestampTimezone       : ZoneId             = ZoneId.systemDefault()
+  archivePath             : Option[String]               = None,
+  mainTitle               : Option[String]               = None,
+  mainTagline             : Option[String]               = None,
+  overrideDisplayName     : Option[String]               = None,
+  newSelfUrl              : Option[String]               = None,
+  themeName               : String                       = "shatter",
+  themeConfig             : immutable.Map[String,String] = immutable.Map.empty,
+  pageLength              : Int                          = 20, // for themes that support paging
+  contentTransformer      : ContentTransformer           = identity,
+  toFollowersAsPublic     : Boolean                      = false,
+  sensitiveAsPublic       : Boolean                      = false,
+  suppressLinksToOriginal : Boolean                      = false,
+  timestampTimezone       : ZoneId                       = ZoneId.systemDefault()
 )
 
